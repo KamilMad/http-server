d7b7f183552ec0ea01a0f13720b829c12622a131
KamilMad <kamilul085@gmail.com>
2026-02-21 14:34:26 +0100

    feat: add simple server response

--- src/main/java/pl/kamil/SimpleHttpServer.java
+++ src/main/java/pl/kamil/SimpleHttpServer.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -21,7 +24,9 @@ public class SimpleHttpServer {
     }
 
     public void start() {
-        try(ServerSocket server = new ServerSocket(8080)) {
+        try(ServerSocket server = new ServerSocket()) {
+            server.setReuseAddress(true);
+            server.bind(new InetSocketAddress(8080));
             logger.info("Server listening on port {}" , 8080);
 
             while (true) {
@@ -38,9 +43,19 @@ public class SimpleHttpServer {
     }
 
     private void handleClient(Socket clientSocket) {
-        try (clientSocket) {
+        try (clientSocket;
+             InputStream in = clientSocket.getInputStream();
+             OutputStream out = clientSocket.getOutputStream()) {
+
             httpRequestParser.parse(clientSocket.getInputStream());
-        } catch (IOException e) {
+            Thread.sleep(3000);
+
+            String response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+            out.write(response.getBytes());
+            out.flush();
+
+            logger.info("Client handled successfully.");
+        } catch (Exception e) {
             logger.error("Error handling client: {}", e.getMessage());
         }
     }
